b8b0a35ae640ec1e0e2d8ea122b35d2dd44031c8
Eric Sabado <eric@teamcodeflux.com>
2011-06-01 17:54:12 +0800

    added mock implementation for quote.random
    Signed-off-by: Eric Sabado <eric@teamcodeflux.com>

--- qotd/grails-app/controllers/qotd/QuoteController.groovy
+++ qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -9,4 +9,10 @@ class QuoteController {
 	def home = { 
 		render "<h1>Real Programmers do not eat Quiche</h1>"
 		}
+	
+	def random = {
+		def staticAuthor = "Anonymous"
+		def staticContent = "Real Programmers do not eat Quiche"
+		[author:staticAuthor, content:staticContent]
+	}
 }
