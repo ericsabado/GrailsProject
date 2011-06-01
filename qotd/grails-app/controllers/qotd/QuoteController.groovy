4cb373ed6d8e34c35dac6ea95f4dd7ed22bc22bf
Eric Sabado <eric@teamcodeflux.com>
2011-06-01 16:33:53 +0800

    added sample content for quote.home, redirecting quote.index to home
    Signed-off-by: Eric Sabado <eric@teamcodeflux.com>

--- qotd/grails-app/controllers/qotd/QuoteController.groovy
+++ qotd/grails-app/controllers/qotd/QuoteController.groovy
@@ -2,5 +2,11 @@ package qotd
 
 class QuoteController {
 
-    def index = { }
+	def index = {
+		redirect(action:home)
+		}
+
+	def home = { 
+		render "<h1>Real Programmers do not eat Quiche</h1>"
+		}
 }
